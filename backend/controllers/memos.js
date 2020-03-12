a352b8b1566c5237f2349476a5a6a70f718dbafb
unknown <alsgks1218@naver.com>
2020-03-12 13:20:59 +0900

    add ObjectId validation Func and Joi for req.body form check

--- backend/controllers/memos.js
+++ backend/controllers/memos.js
@@ -1,6 +1,31 @@
 import Memo from "../models/memo";
+import mongoose from "mongoose";
+import Joi from "@hapi/joi";
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (req, res, next) => {
+  const { id } = req.params;
+  if (!ObjectId.isValid(id)) {
+    res.status(400).send("no!");
+  }
+  return next();
+};
 
 export const write = async (req, res) => {
+  const schema = Joi.object({
+    title: Joi.string().required(),
+    body: Joi.string().required()
+  });
+  const result = schema.validate(req.body);
+  console.log(result);
+  if (result.error) {
+    const { error } = result;
+    return res.status(400).json({ error });
+    // return res.status(404).json({code:404, text: "page not Found"}) 형태로 구성하면 될듯
+  }
+  // Joi의 경우 @hapi/joi로 다운받운 경우 node12가 아니라서 validateAsync가 에러가 걸리는 듯
+  // 또한 책에서와는 달리 schema.validate(값)으로 비교를 했음
   const { title, body } = req.body;
   const memo = new Memo({
     title,
@@ -26,7 +51,6 @@ export const list = async (req, res) => {
 export const read = async (req, res) => {
   const { id } = req.params;
   console.log(id);
-  // res.json({ a: "b" });
   try {
     const memo = await Memo.findById(id);
     if (!memo) {
@@ -38,5 +62,37 @@ export const read = async (req, res) => {
     console.log(e);
   }
 };
-export const update = (req, res) => res.send("update");
-export const remove = (req, res) => res.send("remove");
+export const remove = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Memo.findByIdAndRemove(id);
+    //status만 보내는건 통신이 끝나질 않는다. 실제 값을 보내줘야함
+    res.status(204).send("Removed");
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ e });
+  }
+};
+export const update = async (req, res) => {
+  const { id } = req.params;
+  const schema = Joi.object({
+    title: Joi.string(),
+    body: Joi.string()
+  });
+  const result = schema.validate(req.body);
+  if (result.error) {
+    const { error } = result;
+    return res.status(400).json({ error });
+  }
+  try {
+    // 첫번째 파라미터는 id, 두번째 파라미터는 업데이트 내용, 세번째 파라미터는 업데이트의 옵션
+    const memo = await Memo.findByIdAndUpdate(id, req.body, { new: true });
+    if (!memo) {
+      res.status(404).send("no memo");
+    }
+    res.json({ memo });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ e });
+  }
+};
--- backend/routers/memos.js
+++ backend/routers/memos.js
@@ -1,12 +1,19 @@
 import express from "express";
-import { read, write, update, list, remove } from "../controllers/memos";
+import {
+  read,
+  write,
+  update,
+  list,
+  remove,
+  checkObjectId
+} from "../controllers/memos";
 
 const memosRouter = express.Router();
 
 memosRouter.get("/", list);
 memosRouter.post("/", write);
-memosRouter.get("/:id", read);
-memosRouter.delete("/:id", remove);
-memosRouter.patch("/:id", update);
+memosRouter.get("/:id", checkObjectId, read);
+memosRouter.delete("/:id", checkObjectId, remove);
+memosRouter.patch("/:id", checkObjectId, update);
 
 export default memosRouter;
