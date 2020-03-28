462c2824f7e9dd8a23e392925c6d54a6301fdb31
minhan <alsgks1218@gmail.com>
2020-03-29 00:42:51 +0900

    add palette(open-color) and button component

--- frontend/src/pages/MemoListPage.js
+++ frontend/src/pages/MemoListPage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Button from "../components/ButtonComponent";
 
 const MemoListPage = () => {
-  return <div>메모 리스트</div>;
+  return (
+    <div>
+      <Button>버튼</Button>
+    </div>
+  );
 };
 
 export default MemoListPage;
